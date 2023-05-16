bc8a7985b2878dbd60a58f2c70dc978109f519a7
Nelson Corredera <ngcorr97@gmail.com>
2023-05-16 20:56:23 -0300

    agregado de array-objeto y funcion busqueda

--- main.js
+++ main.js
@@ -3,49 +3,39 @@ function nombre(producto) {
     return producto;
 }
 
-let torno = 10000;
-let centro = 20000;
-let brazo = 30000;
-let valor = 0;
+const maquinas = [
+    { id: 1, tipo: "BR-6743", nombre: "brazo con rotacion", precio: 10000 },
+    { id: 2, tipo: "FA-9873", nombre: "centro de mecanizado", precio: 20000 },
+    { id: 3, tipo: "KS-2124", nombre: "torno", precio: 30000 },
+    { id: 4, tipo: "BR-1213", nombre: "brazo con movimiento lineal", precio: 40000 },
+    { id: 5, tipo: "LU-5562", nombre: "centro de mecanizado con opcional", precio: 50000 },
+    { id: 6, tipo: "TA-4353", nombre: "torno con opcional", precio: 60000 },
+];
+
+
 let final = 0;
 let val = 0;
-
 
 let producto = prompt("Ingrese producto a consultar, coloque salir para terminar");
 
 while (producto != "salir") {
 
-    switch (producto.toLowerCase()) {
-
-        case "torno":
-
-            valor = nombre(torno);
-            break;
-
-        case "centro":
-
-            valor = nombre(centro);
-            break;
-
-        case "brazo":
-
-            valor = nombre(brazo);
-            break;
-    }
-
+    const busqueda = maquinas.find((maquina) => maquina.tipo.toLowerCase() === producto.toLowerCase());
+    alert(`la maquina seleccionada es ${busqueda.nombre}, el precio es de ${busqueda.precio}`)
     let descuento = prompt("de tener ingrese codigo de descuento");
     if (descuento.toLowerCase() == "bajo") {
 
-        val = valor * 0.8;
+        val = busqueda.precio * 0.8;
         alert(`se aplica descuento del 20% quedando ${val}`);
 
     }
     else {
 
-        val = valor;
+        val = busqueda.precio;
 
     }
-
+    
+    
     producto = prompt("Ingrese producto a consultar, coloque salir para terminar");
 
     final = final + val;
@@ -58,6 +48,7 @@ if (condicion.toLowerCase() == "si") {
     alert(`precio total de maquina consultadas es de ${final}`);
 
 }
+
 alert(`Gracias por su visita`);
 
 
